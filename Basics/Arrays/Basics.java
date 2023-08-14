44e96c918c7e0a27154baa40096c91b3be0ffc32
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-14 10:36:38 +0000

    Basics of array

--- Basics/Arrays/Basics.java
+++ Basics/Arrays/Basics.java
@@ -6,20 +6,28 @@ public class Basics {
         // // Syntax
         // datatype[] variable_name = new dataype[size];
 
-        int[] rnos = new int[6];
+        // int[] rnos = new int[6];
 
         // or directly
 
-        int[] rnos2 = {23 , 4 ,5};
+        // int[] rnos2 = {23 , 4 ,5};
 
-        int[] ros;   // decleration of array. ros is getting defined in the stack
-       ros = new int[5];  //initialisation: actually here object is being created in the heap 
+        // int[] ros; // decleration of array. ros is getting defined in the stack
+        // ros = new int[5]; //initialisation: actually here object is being created in
+        // the heap
 
-    //  System.out.println(ros[1]);
-    String[] arr = new String[4];
-    System.out.println(arr[0]);
+        // System.out.println(ros[1]);
+        // String[] arr = new String[4];
+        // System.out.println(arr[0]);
+
+        // you cannot assign a null (it is a literal ) to any premitive datatype u just
+        // assigned it to a non primitive datatype.
+
+        // example
+        // String str = null;(right)
+        //  int num = null;(Wrong)
+
+        // any ref variable that u have bydefault its going to be of null
 
     }
-    }
-
-
+}
