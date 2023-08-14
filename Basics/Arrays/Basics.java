40e5e50d43fec54632af410ee7a140a1daa797c7
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-14 10:35:58 +0000

    Basics about Array

---
+++ Basics/Arrays/Basics.java
@@ -0,0 +1,25 @@
+package Basics.Arrays;
+
+public class Basics {
+    public static void main(String[] args) {
+
+        // // Syntax
+        // datatype[] variable_name = new dataype[size];
+
+        int[] rnos = new int[6];
+
+        // or directly
+
+        int[] rnos2 = {23 , 4 ,5};
+
+        int[] ros;   // decleration of array. ros is getting defined in the stack
+       ros = new int[5];  //initialisation: actually here object is being created in the heap 
+
+    //  System.out.println(ros[1]);
+    String[] arr = new String[4];
+    System.out.println(arr[0]);
+
+    }
+    }
+
+
