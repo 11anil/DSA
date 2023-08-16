1151326f80221ba6eb449589d706d919fc2893a4
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-16 16:44:52 +0000

    Updated version

--- Basics/Arrays/Twod.java
+++ Basics/Arrays/Twod.java
@@ -1,22 +1,39 @@
 package Basics.Arrays;
+
 import java.util.*;
+
 public class Twod {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] arr = new int [3][3];
-        for(int row = 0; row<arr.length; row++){
-            for(int col=0; col<arr[row].length; col++){
+        int[][] arr = new int[3][3];
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
                 arr[row][col] = sc.nextInt();
             }
 
         }
-       for(int row =0; row<arr.length; row++){
-        for(int col=0; col<arr[row].length; col++){
-            System.out.print(arr[row][col] + " ");
+
+        // Output
+        // for(int row =0; row<arr.length; row++){
+        // for(int col=0; col<arr[row].length; col++){
+        // System.out.print(arr[row][col] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // Another way to do that
+
+        // for (int row = 0; row < arr.length; row++) {
+        //     System.out.println(Arrays.toString(arr[row]));
+        // }
+
+        //note: Datatype of every single lement in a array is itself array
+
+        //by enhanced for loop
+        for(int[] a: arr){
+            System.out.println(Arrays.toString(a));
         }
-        System.out.println();
-       }
-       sc.close();
+        sc.close();
     }
 }
