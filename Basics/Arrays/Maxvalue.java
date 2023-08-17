fc8fe3e12e783b814c80f892e48351c1029cd218
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-17 05:19:45 +0000

    maxvalue in a range

--- Basics/Arrays/Maxvalue.java
+++ Basics/Arrays/Maxvalue.java
@@ -3,7 +3,7 @@ package Basics.Arrays;
 public class Maxvalue {
     public static void main(String args[]){
         int[]arr = {1,3,23,4,45};
-        System.out.println(max(arr));
+        System.out.println (maxrange(arr,2,4));
     }
     static int max(int[] arr){
        int maxval = arr[0];
@@ -14,4 +14,16 @@ public class Maxvalue {
     }
     return maxval;
 }
+
+static int maxrange(int[] arr,int start ,int end){
+ int maxrange = arr[3];
+ for(int i=start; i<end; i++){
+    if(maxrange<arr[i]){
+        maxrange = arr[i];
+    }
+ }
+ return maxrange;
+}
+
+
 }
