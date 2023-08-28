cd17056ec7b2a5613e7628b81efe6017402ad711
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-28 06:13:55 +0000

    PrettyPrinting in java

---
+++ Basics/StringAndStringBuilders/PrettyPrinting.java
@@ -0,0 +1,13 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        // float a = 234.1274f;
+        // System.out.printf("The fornmated number is %.2f",a);
+
+
+        // System.out.printf("Pie is %.3f",Math.PI);
+        System.out.printf("Pie is %.3f%n", Math.PI);
+
+        System.out.printf("Hello my name is %s and i am %s","Anil","Cool");
+
+    }
+}
