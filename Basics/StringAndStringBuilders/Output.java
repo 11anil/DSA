c4dfd59d2086f18a8a3c90be3592b6bb94e7f9e1
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-28 05:54:11 +0000

    Different typess of outputs in String (Basics)

---
+++ Basics/StringAndStringBuilders/Output.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+
+       Integer num = new Integer(56);
+        System.out.println(num.toString());
+        // System.out.println("Ani");
+        // System.out.println(Arrays.toString(new int[]{2,4,6,8}));
+
+        // String name = null;
+        // System.out.println(name);
+    }
+}
